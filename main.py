86e30009c64b82f286f0aa0168fcf4d6cc5eaca9
Thayanne França da Silva <thayanne.silva@dellead.com>
2022-06-02 11:04:52 -0300

    Adicionando método post para previsão de preços da casa para multiplas variaveis

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from textblob import TextBlob
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('casas.csv')
-colunas = ['tamanho', 'preco']
-df = df[colunas]
+colunas = ['tamanho', 'ano', 'garagem']
 
 X = df.drop('preco', axis=1)
 y = df['preco']
@@ -30,9 +29,11 @@ def sentimento(frase):
 
     return "polaridade: {}".format(polaridade)
 
-@app.route('/cotacao/<int:tamanho>')
-def cotacao(tamanho):
-    preco = modelo.predict([[tamanho]])
-    return str(preco)
+@app.route('/cotacao/', methods=['POST'])
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])
 
 app.run(debug=True)
